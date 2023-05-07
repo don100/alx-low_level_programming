88decb0da01a9157a538ea19f0dc999f7259e159
don100 <hajjaj.chiheb1@gmail.com>
2023-05-07 04:14:53 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,13 @@
  *
  * Return:converted number, or 0
  */
-unsigned int binary_to_uint(const char *b)
-{
-	int i;
-	unsigned int j;
 
-	j = 0;
-	if (!b)
-		return (0);
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		j <<= 1;
-		if (b[i] == '1')
-			j += 1;
-	}
-	return (j);
+unsigned int binary_to_uint(char *a){
+    unsigned int x = 0;
+    for(int i =0 ; i<strlen(a) ; i++){
+        if(a[i]!='0' && a[i]!='1')
+            return 0;
+        x+=(a[i]-'0')*pow(2,strlen(a)-i-1);
+    }
+ return x;   
 }
